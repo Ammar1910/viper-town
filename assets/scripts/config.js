747206043b608aa9bbd3843ba3b3ca51b962097a
unknown <you@example.com>
2024-12-31 05:58:11 +0200

    first commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,27 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "0.0.0.0:30120";
+
+Config.Socials = [
+    { name: "discord", label: "Discord", description: "نحن في انتظارك على خادم الدسكورد !!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/8nruRjEnmz" },
+];
+
+Config.HideoverlayKeybind = 112
+Config.CustomBindText = "F1";
+
+// Staff list
+Config.Staff = [
+    { name: "Ali Al Tamimi", description: "Developer", color: "#fff", image: "https://imgur.com/KTLPFqS" },
+
+
+
+];
+
+// Categories
+Config.Categories = [
+    { label: "وسائل التواصل الاجتماعي", default: true },
+    { label: "طاقم", default: false }
+];
+
+// Music
+Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'ALI AL TAMIMI'
+description 'ATSTORE'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+loadscreen 'index.html'
